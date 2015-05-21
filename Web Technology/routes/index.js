be067eb0a2d44745680262bf7c61a76881df911d
Xibo Wang <paulop5288@gmail.com>
2015-05-21 23:18:17 +0100

    Add changable input text field

--- Web Technology/routes/index.js
+++ Web Technology/routes/index.js
@@ -11,31 +11,30 @@ var client = new twit({
   access_token: '3288379547-Em8DIkNc1mmoKQqoDUAs8mCxahQ6jMNduAxu5Go',
   access_token_secret: 'gHzlapAhUjuzCgpAaI8t5oQdzJTIwy7yjP9IowJClkgFB'
 });
-var tweets = {
-  "tweets" : ["tweet A", "tweet B", "Tweet C"]
-}
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log( tweets);
-  var twitQuery = req.query.query;;
-  if (twitQuery == null) {
-    res.render('index', { title: 'Express',tweets: [] });
-  } else {
-    console.log("twit query : " + twitQuery);
-    res.render('index', _.extend({ title: 'Express'},tweets));
-    client.get("search/tweets", { q: twitQuery, count: 1},
-    function(err, data, res) {
-      for (var count in data.statuses) {
-        var tweet= data.statuses[count];
-        console.log('on: ' + tweet.created_at + ' : @'
-            + tweet.user.screen_name + ' : '
-            + tweet.text+'\n\n');
-      }
-    });
-  }
-
+    // array of tweet result
+    var tweets = [];
+    var twitQuery = req.query.query;
+    if (twitQuery == null) {
+        res.render('index', {title: 'Search Twitter', "tweets": []});
+    } else {
+        client.get("search/tweets", {q: twitQuery, count: 0},
+            function (err, data) {
+                for (var count in data.statuses) {
+                    var tweet = data.statuses[count];
+                    // add each tweet to array
+                    tweets.push(tweet.text);
+                    console.log(count + ": " + tweets[count]);
+                    //console.log('on: ' + tweet.created_at + ' : @'
+                    //    + tweet.user.screen_name + ' : '
+                    //    + tweet.text+'\n\n');
+                }
+                res.render('index', _.extend({ title: 'Search Twitter'},{"tweets": tweets}));
+            });
+    }
 });
 
 /* GET hello world page. */
